67cdde2b68dcffcfc7a237ed1105db8baad9d5a0
LuigiVeiga <luigiveiga@alunos.utfpr.edu.br>
2025-04-09 09:06:39 -0300

    Add files via upload

---
+++ taxi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float A, G, Ra, Rg;
+    printf("digite o preço do litro de álcool e da gasolina respectivamente. Em segida digite o rendimentos de ambos em km/l\n");
+    scanf("%f %f %f %f", &A, &G, &Ra, &Rg);
+    if ((Ra/A)>(Rg/G))
+    printf("a tropa de taxi deve abastecer com álcool");
+    else
+    printf("a tropa de taxi deve abastecer a frota com gasolina");
+    return 0;
+}

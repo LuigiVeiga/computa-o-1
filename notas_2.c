49e29c70b9064e7b5f3a56dbc6f1a4f11476be4c
LuigiVeiga <luigiveiga@alunos.utfpr.edu.br>
2025-04-09 08:41:17 -0300

    Add files via upload

--- notas_2.c
+++ notas_2.c
@@ -5,16 +5,16 @@ int main()
     printf("Digite as notas de cada trimestre");
     scanf("%f %f %f", &n1, &n2, &n3);
     int fr;
-    print("digite sua frequencia");
+    printf("digite sua frequencia");
     scanf("%d", &fr);
     media=(n1+n2+n3)/3;
     if (media>8 && fr>= 75)
-        printf("O estudante foi aprovado com Distinção.Pois teve média %.1f e %d",media, fr);
+        printf("O estudante foi aprovado com Distinção.Pois teve média %.1f e frequencia %d\n",media, fr);
     else if(media>=6 && fr>=75)
-        printf("O estudante foi aprovado direto.Pois teve média %.1f e %d",media, fr);
+        printf("O estudante foi aprovado direto.Pois teve média %.1f e frequencia %d\n",media, fr);
     else if(media>=4 &&  fr>=75 || media<6 && fr <75 || media >=4 && fr>50 && fr <75)
-        printf("O estudante foi para final.Pois teve média %.1f e %d",media, fr);
+        printf("O estudante foi para final.Pois teve média %.1f e frequencia %d\n",media, fr);
     else
-        printf("O estudante foi reprovado direto.Pois teve média %.1f e %d",media, fr);
+        printf("O estudante foi reprovado direto.Pois teve média %.1f e frequencia %d\n",media, fr);
     return 0;
 }

25ae83f63fa86f16e66d84cafb08d57c9f0ef737
LuigiVeiga <luigiveiga@alunos.utfpr.edu.br>
2025-04-09 08:32:04 -0300

    Add files via upload

---
+++ notas_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float n1, n2, n3, media;
+    printf("Digite as notas de cada trimestre");
+    scanf("%f %f %f", &n1, &n2, &n3);
+    int fr;
+    print("digite sua frequencia");
+    scanf("%d", &fr);
+    media=(n1+n2+n3)/3;
+    if (media>8 && fr>= 75)
+        printf("O estudante foi aprovado com Distinção.Pois teve média %.1f e %d",media, fr);
+    else if(media>=6 && fr>=75)
+        printf("O estudante foi aprovado direto.Pois teve média %.1f e %d",media, fr);
+    else if(media>=4 &&  fr>=75 || media<6 && fr <75 || media >=4 && fr>50 && fr <75)
+        printf("O estudante foi para final.Pois teve média %.1f e %d",media, fr);
+    else
+        printf("O estudante foi reprovado direto.Pois teve média %.1f e %d",media, fr);
+    return 0;
+}
